9bbecf34a673ff00c11b017be17f24e5d8dfc029
mugiwarakufi <112404865+mugiwarakufi@users.noreply.github.com>
2022-09-02 09:26:34 -0500

    Update adding_numbers.py

--- adding_numbers.py
+++ adding_numbers.py
@@ -1,1 +1,11 @@
+def add(n1, n2):
+  return float(n1) + float(n2)
+  
+n1 = input("Enter a number: ")
+n2 = input("Enter a number: ")
 
+if len(str(n1)) == 0 or len(str(n2)) == 0:
+  print("Invalid Operation")
+   
+if len(str(n1)) != 0 or len(str(n1)) != 0:
+  print(add(n1, n2))
