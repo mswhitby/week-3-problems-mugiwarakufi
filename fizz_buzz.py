81df6ea0dcb827eeba0f2c4af5103b1bb0173157
mugiwarakufi <112404865+mugiwarakufi@users.noreply.github.com>
2022-09-02 09:34:22 -0500

    Update fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,1 +1,13 @@
+buzzbuzzbuzz = int(input("Enter an integer: "))
 
+def fizz_buzz(buzzbuzzbuzz):
+  if buzzbuzzbuzz % 3 == 0 and buzzbuzzbuzz % 5 == 0:
+    print("FizzBuzz")
+  elif buzzbuzzbuzz % 3 == 0:
+    print("Fizz")
+  elif buzzbuzzbuzz % 5 == 0:
+    print("Buzz")
+  else:
+    print(buzzbuzzbuzz)
+  
+fizz_buzz(buzzbuzzbuzz)
