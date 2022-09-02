5935b66fab2c94ec593fb7a8f976095ddeb57b2f
mugiwarakufi <112404865+mugiwarakufi@users.noreply.github.com>
2022-09-02 09:32:59 -0500

    Update list_of_multiples.py

--- list_of_multiples.py
+++ list_of_multiples.py
@@ -1,1 +1,6 @@
-
+actor = int(input("What factor would you like a list of multiples of? This must must be an integer: "))
+length = int(input("Enter how long you would like your list of factors to be. This number must be an integer greater than 0: "))
+def list_of_multiples(factor, length):
+    return range(factor, length*factor+1, factor)
+  
+print list_of_multiples(factor, length)
