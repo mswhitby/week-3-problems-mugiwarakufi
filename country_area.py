265e6d158527c2f70d6cb9855033fe6331a187f0
mugiwarakufi <112404865+mugiwarakufi@users.noreply.github.com>
2022-09-06 09:11:17 -0500

    Update country_area.py

--- country_area.py
+++ country_area.py
@@ -1,1 +1,11 @@
+country = input("country name: ")
+area = float(input("enter area of country (in square kilometers - refrain from using commas in your input): "))
 
+def area_of_country(country, area):
+  landmass = 148940000
+  proportion = area * 100 / landmass
+  
+  sentence = "{} is {:.2f} percent of the total world's landmass".format(country,proportion)
+  print(sentence)
+  
+area_of_country(country, area)
