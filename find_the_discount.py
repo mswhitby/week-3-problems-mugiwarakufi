62df22a391e69870fa8fc5eb8a7f642ffeae0327
mugiwarakufi <112404865+mugiwarakufi@users.noreply.github.com>
2022-09-06 09:33:53 -0500

    Update find_the_discount.py

--- find_the_discount.py
+++ find_the_discount.py
@@ -1,1 +1,10 @@
+price = int(input("Enter the original price of our item or combined price of your items. This number should be a whole number. "))
+discount = int(input("Enter the rate of the discount. This number should be a whole number between 0 and 100. "))
 
+def dis(price, discount):
+  final_price = price * (100 - discount) / 100
+  
+  disfinal = "{:.2f}".format(final_price)
+  print(disfinal)
+  
+dis(price, discount)
